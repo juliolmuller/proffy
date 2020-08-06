b74a3af1c92733a1bc9ac225bcd0756ad853c8fd
Julio Muller <juliodolago@gmail.com>
2020-08-05 23:28:34 -0300

    Moved request processing action into controller method

---
+++ back-end/src/controllers/ClassController.ts
@@ -0,0 +1,42 @@
+import { Request, Response, response } from 'express'
+import database from '../database/connection'
+import parseTimeIntoMinutes from '../utils/parseTimeIntoMinutes'
+
+interface FriendlySchedule {
+  weekday: number
+  from: string
+  to: string
+}
+
+class ClassController {
+
+  async create(request: Request, response: Response) {
+    const { name, avatar, whatsapp, bio, subject, price, schedule } = request.body
+    const transaction = await database.transaction()
+
+    try {
+      /* eslint-disable camelcase */
+      const [user_id] = await transaction('users').insert({ name, avatar, whatsapp, bio })
+      const [class_id] = await transaction('classes').insert({ subject, price, user_id })
+      /* eslint-enable camelcase */
+      await transaction('class_schedules').insert(schedule.map((sch: FriendlySchedule) => ({
+        weekday: sch.weekday,
+        from: parseTimeIntoMinutes(sch.from),
+        to: parseTimeIntoMinutes(sch.to),
+        class_id,
+      })))
+
+      await transaction.commit()
+
+      response.status(201).send()
+    } catch (error) {
+      transaction.rollback(error)
+
+      response.status(400).json({
+        error: 'Unexpected error while creating user, class or schedules.',
+      })
+    }
+  }
+}
+
+export default ClassController
--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,37 +1,11 @@
 import express from 'express'
-import database from './database/connection'
-import parseTimeIntoMinutes from './utils/parseTimeIntoMinutes'
+import ClassController from './controllers/ClassController'
 
 const routes = express.Router()
+const classCtrl = new ClassController()
 
 routes.get('/', (req, res) => res.json({ message: 'Hello, there!' }))
 
-routes.post('/classes', async (req, res) => {
-  const { name, avatar, whatsapp, bio, subject, price, schedule } = req.body
-  const transaction = await database.transaction()
-
-  try {
-    /* eslint-disable camelcase */
-    const [user_id] = await transaction('users').insert({ name, avatar, whatsapp, bio })
-    const [class_id] = await transaction('classes').insert({ subject, price, user_id })
-    /* eslint-enable camelcase */
-    await transaction('class_schedules').insert(schedule.map((sch: any) => ({
-      weekday: sch.weekday,
-      from: parseTimeIntoMinutes(sch.from),
-      to: parseTimeIntoMinutes(sch.to),
-      class_id,
-    })))
-
-    await transaction.commit()
-
-    res.status(201).send()
-  } catch (error) {
-    transaction.rollback(error)
-
-    res.status(400).json({
-      error: 'Unexpected error while creating user/class.',
-    })
-  }
-})
+routes.post('/classes', classCtrl.create)
 
 export default routes

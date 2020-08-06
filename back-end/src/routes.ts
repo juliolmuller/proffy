f5ee84323e978b67c26f03ee68344743f040c1aa
Julio Muller <juliodolago@gmail.com>
2020-08-05 22:12:16 -0300

    Added try-catch surrounging database operations

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -10,20 +10,28 @@ routes.post('/classes', async (req, res) => {
   const { name, avatar, whatsapp, bio, subject, price, schedule } = req.body
   const transaction = await database.transaction()
 
-  /* eslint-disable camelcase */
-  const [user_id] = await transaction('users').insert({ name, avatar, whatsapp, bio })
-  const [class_id] = await transaction('classes').insert({ subject, price, user_id })
-  /* eslint-enable camelcase */
-  await transaction('class_schedules').insert(schedule.map((sch: any) => ({
-    weekday: sch.weekday,
-    from: parseTimeIntoMinutes(sch.from),
-    to: parseTimeIntoMinutes(sch.to),
-    class_id,
-  })))
+  try {
+    /* eslint-disable camelcase */
+    const [user_id] = await transaction('users').insert({ name, avatar, whatsapp, bio })
+    const [class_id] = await transaction('classes').insert({ subject, price, user_id })
+    /* eslint-enable camelcase */
+    await transaction('class_schedules').insert(schedule.map((sch: any) => ({
+      weekday: sch.weekday,
+      from: parseTimeIntoMinutes(sch.from),
+      to: parseTimeIntoMinutes(sch.to),
+      class_id,
+    })))
 
-  await transaction.commit()
+    await transaction.commit()
 
-  res.status(201).send()
+    res.status(201).send()
+  } catch (error) {
+    transaction.rollback(error)
+
+    res.status(400).json({
+      error: 'Unexpected error while creating user/class.',
+    })
+  }
 })
 
 export default routes

b6d37bd967d5eb9a13819a26d5676ee9debd3906
Julio Muller <juliodolago@gmail.com>
2020-08-06 23:16:13 -0300

    Create input components: Select and TextArea

---
+++ front-end/src/components/Select/index.tsx
@@ -0,0 +1,23 @@
+import React, { SelectHTMLAttributes } from 'react'
+import './styles.css'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string
+  label: string
+  options: Array<{
+    value: string
+    label: string
+  }>
+}
+
+const Select: React.FC<SelectProps> = ({ name, label, options, ...attr }) => (
+  <div className="select-group">
+    <label htmlFor={name}>{label}</label>
+    <select id={name} defaultValue="" {...attr}>
+      <option value="" disabled hidden>Selecione...</option>
+      { options.map(opt => <option key={opt.value} value={opt.value}>{opt.label}</option>) }
+    </select>
+  </div>
+)
+
+export default Select
---
+++ front-end/src/components/TextArea/index.tsx
@@ -0,0 +1,16 @@
+import React, { TextareaHTMLAttributes } from 'react'
+import './styles.css'
+
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  name: string
+  label: string
+}
+
+const TextArea: React.FC<TextAreaProps> = ({ name, label, ...attr }) => (
+  <div className="textarea-group">
+    <label htmlFor={name}>{label}</label>
+    <textarea id={name} {...attr} />
+  </div>
+)
+
+export default TextArea

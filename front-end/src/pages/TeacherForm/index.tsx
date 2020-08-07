a55d46abfafee8b1ca4089b7ff5d91e44e3e0155
Julio Muller <juliodolago@gmail.com>
2020-08-06 22:05:20 -0300

    Created teacher's form (most of it)

--- front-end/src/pages/TeacherForm/index.tsx
+++ front-end/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PageHeader from '../../components/PageHeader'
+import InputField from '../../components/InputField'
+import warningIcon from '../../assets/images/icons/warning.svg'
 import './styles.css'
 
 const TeacherForm = () => (
@@ -8,6 +10,30 @@ const TeacherForm = () => (
       title="Que incrível que você quer dar aulas!"
       description="O primeiro passo é preencher este formulário de inscrição"
     />
+    <main>
+      <fieldset>
+        <legend>Seus dados</legend>
+        <InputField type="text" name="name" label="Nome completo:" />
+        <InputField type="text" name="avatar" label="Avatar:" />
+        <InputField type="text" name="whatsapp" label="WhatsApp:" />
+      </fieldset>
+      <fieldset>
+        <legend>Sobre a aula</legend>
+        <InputField type="text" name="subject" label="Matéria:" />
+        <InputField type="text" name="price" label="Valor da sua hora:" />
+      </fieldset>
+
+      <footer>
+        <p>
+          <img src={warningIcon} alt="Alerta" />
+          Importante! <br/>
+          Preencha todos os dados.
+        </p>
+        <button type="button">
+          Cadastrar
+        </button>
+      </footer>
+    </main>
   </div>
 )
 

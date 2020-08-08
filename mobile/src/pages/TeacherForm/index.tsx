6c4cc4a4bb2384f01340a8e333c5cb24ce6cb91d
Julio Muller <juliodolago@gmail.com>
2020-08-08 19:27:09 -0300

    Finished 'TeacherForm' page

--- mobile/src/pages/TeacherForm/index.tsx
+++ mobile/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 import styles from './styles'
+import giveClassesBackground from '../../assets/images/give-classes-background.png'
 
-const TeacherForm = () => (
-  <View style={styles.container}>
-    <Text style={styles.headline}>
-      Formulário de Cadastro de Professor
-    </Text>
-  </View>
-)
+const TeacherForm = () => {
+  const { goBack } = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBackground}
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.headline}>
+          Quer ser um Proffy?
+        </Text>
+        <Text style={styles.description}>
+          Para começar a dar aulas, você precisa se cadastrá-las em nossa plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={goBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Entendi</Text>
+      </RectButton>
+    </View>
+  )
+}
 
 export default TeacherForm
--- mobile/src/pages/TeacherForm/styles.ts
+++ mobile/src/pages/TeacherForm/styles.ts
@@ -9,8 +9,39 @@ export default StyleSheet.create({
     padding: 40,
   },
 
+  content: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+
   headline: {
+    maxWidth: 200,
     color: '#fff',
-    fontSize: 20,
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 45,
+    lineHeight: 50,
+  },
+
+  description: {
+    marginTop: 24,
+    color: '#d4c2ff',
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+
+  okButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 58,
+    marginVertical: 40,
+    backgroundColor: '#04d361',
+    borderRadius: 8,
+  },
+
+  okButtonText: {
+    color: '#fff',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16,
   },
 })

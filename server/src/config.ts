2ed7ed809980704ecfa2327f1a9d5de782c0a15b
Julio Muller <juliodolago@gmail.com>
2020-12-24 00:43:31 -0300

    Configured parsers for Postgres' data types

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,21 @@
 import dotenv from 'dotenv-flow'
+import pg from 'pg'
 
+/**
+ * Setup environment variables
+ */
 const config = dotenv.config({ purge_dotenv: true })
 
 if (config.error && process.env.NODE_ENV !== 'production') {
   throw config.error
 }
+
+/**
+ * Configure PostgreSQL types parsers used by Knex
+ */
+const types = pg.types.builtins
+const parser = (value: string) => (value === null ? null : Number(value))
+
+pg.types.setTypeParser(types.INT4, parser)
+pg.types.setTypeParser(types.INT8, parser)
+pg.types.setTypeParser(types.NUMERIC, parser)

c11749e142506844e61bce05a6243d2af4c5d486
Julio Muller <juliodolago@gmail.com>
2020-12-25 22:57:57 -0300

    Added link to web version of the app

--- mobile/src/pages/TeacherForm/index.tsx
+++ mobile/src/pages/TeacherForm/index.tsx
@@ -1,33 +1,28 @@
 import React, { FC } from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import { ImageBackground, Linking, Text, View } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
-import { useNavigation } from '@react-navigation/native'
 import giveClassesBackground from '../../images/give-classes-background.png'
 import styles from './styles'
 
-const TeacherForm: FC = () => {
-  const { goBack } = useNavigation()
+const TeacherForm: FC = () => (
+  <View style={styles.container}>
+    <ImageBackground
+      source={giveClassesBackground}
+      style={styles.content}
+      resizeMode="contain"
+    >
+      <Text style={styles.headline}>
+        Quer ser um Proffy?
+      </Text>
+      <Text style={styles.description}>
+        Para começar a dar aulas, você precisa se cadastrá-las em nossa plataforma web.
+      </Text>
+    </ImageBackground>
 
-  return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={giveClassesBackground}
-        style={styles.content}
-        resizeMode="contain"
-      >
-        <Text style={styles.headline}>
-          Quer ser um Proffy?
-        </Text>
-        <Text style={styles.description}>
-          Para começar a dar aulas, você precisa se cadastrá-las em nossa plataforma web.
-        </Text>
-      </ImageBackground>
-
-      <RectButton onPress={goBack} style={styles.okButton}>
-        <Text style={styles.okButtonText}>Entendi</Text>
-      </RectButton>
-    </View>
-  )
-}
+    <RectButton onPress={() => Linking.openURL('https://proffy-app-nlw.herokuapp.com')} style={styles.okButton}>
+      <Text style={styles.okButtonText}>Leve-me até lá!</Text>
+    </RectButton>
+  </View>
+)
 
 export default TeacherForm

41fa44cca394ad0800ec74702fd3fb6b63517997
Julio Muller <juliodolago@gmail.com>
2020-12-28 09:31:43 -0300

    Added version numbers to builds

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv-flow'
+import npmConfig from './package.json'
 
 const config = dotenv.config({ purge_dotenv: true })
 
@@ -9,26 +10,35 @@ if (config.error) {
 export default {
   expo: {
     name: 'Proffy',
-    version: '1.0.2',
+    version: npmConfig.version,
+    extra: {
+      ...config.parsed,
+    },
     orientation: 'portrait',
+    platforms: ['android', 'ios'],
     icon: './src/images/favicon.png',
     splash: {
       backgroundColor: '#9871F5',
       image: './src/images/splash.png',
       resizeMode: 'cover',
     },
-    updates: {
-      fallbackToCacheTimeout: 0,
-    },
     assetBundlePatterns: [
       '**/*',
     ],
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    android: {
+      package: 'juliolmuller.proffy',
+      versionCode: 2,
+    },
     ios: {
       supportsTablet: true,
+      bundleIdentifier: 'juliolmuller.proffy',
+      buildNumber: '1.0.0',
     },
     web: {
       favicon: './src/images/favicon.png',
     },
-    extra: { ...process.env },
   },
 }

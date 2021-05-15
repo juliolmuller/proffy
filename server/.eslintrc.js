d242d7acfd917a9e8e1013980f0efa9a51086444
Julio Muller <juliodolago@gmail.com>
2021-05-15 14:18:22 -0300

    Changed linting configurations

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -5,23 +5,14 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    '@lacussoft',
+    'lacussoft',
+    'lacussoft/typescript',
   ],
-  plugins: [
-    '@typescript-eslint',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
   rules: {
     'camelcase': ['error', { allow: ['purge_dotenv', 'user_id', 'class_id'] }],
     'class-methods-use-this': 'off',
     'func-style': 'off',
     'no-console': 'off',
-    'no-magic-numbers': ['error', { ignore: [3030] }],
+    'no-magic-numbers': ['error', { ignore: [8081] }],
   },
 }

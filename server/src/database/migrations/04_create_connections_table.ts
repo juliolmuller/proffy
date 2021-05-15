12eaf0cbd382980f356454ec854cab68ba134362
Julio Muller <juliodolago@gmail.com>
2021-05-15 17:07:40 -0300

    Added constraint for foreign keys

--- server/src/database/migrations/04_create_connections_table.ts
+++ server/src/database/migrations/04_create_connections_table.ts
@@ -6,14 +6,12 @@ export const TABLE_NAME = 'connections'
 export async function up(knex: Knex) {
   return knex.schema.createTable(TABLE_NAME, (table) => {
     table.bigIncrements('id').primary()
-    table.bigInteger('user_id')
-      .notNullable()
+    table.bigInteger('user_id').notNullable().unsigned()
       .references('id')
       .inTable('users')
       .onUpdate('cascade')
       .onDelete('cascade')
-    table.timestamp('created_at')
-      .notNullable()
+    table.timestamp('created_at').notNullable()
       .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
   })
 }

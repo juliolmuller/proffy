0772ecc00a4dca571ec8f429479b5ab3fae51770
Julio Muller <juliodolago@gmail.com>
2021-05-15 14:10:43 -0300

    Dropped routes to static resources

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import './config'
-import path from 'path'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
@@ -13,8 +12,6 @@ app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(routes)
-app.use('/static', express.static(path.join(__dirname, '..', 'public', 'static')))
-app.get('*', (_req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')))
 app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {

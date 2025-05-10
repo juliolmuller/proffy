652b3359db5d90ea2d9818b80592f7e1fc01c626
juliolmuller <juliolmuller@outlook.com>
2025-05-10 00:40:23 -0300

    refactor(server): migrate database to SQLite

--- server/prisma/migrations/20250430045237_migrate_schema/migration.sql
+++ server/prisma/migrations/20250430045237_migrate_schema/migration.sql
@@ -1,52 +1,50 @@
 -- CreateTable
-CREATE TABLE `class_schedules` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `weekday` INTEGER NOT NULL,
-    `from` INTEGER NOT NULL,
-    `to` INTEGER NOT NULL,
-    `class_id` BIGINT UNSIGNED NOT NULL,
+CREATE TABLE "class_schedules" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "weekday" INTEGER NOT NULL,
+  "from" INTEGER NOT NULL,
+  "to" INTEGER NOT NULL,
+  "class_id" TEXT NOT NULL,
 
-    INDEX `class_schedules_class_id_foreign`(`class_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `classes` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `subject` VARCHAR(255) NOT NULL,
-    `price` DECIMAL(8, 2) NOT NULL,
-    `user_id` BIGINT UNSIGNED NOT NULL,
-
-    INDEX `classes_user_id_foreign`(`user_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  CONSTRAINT "class_schedules_class_id_fkey"
+    FOREIGN KEY ("class_id")
+    REFERENCES "classes" ("id")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
 -- CreateTable
-CREATE TABLE `connections` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` BIGINT UNSIGNED NOT NULL,
-    `created_at` TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+CREATE TABLE "classes" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "subject" TEXT NOT NULL,
+  "price" DECIMAL NOT NULL,
+  "user_id" TEXT NOT NULL,
 
-    INDEX `connections_user_id_foreign`(`user_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  CONSTRAINT "classes_user_id_fkey"
+    FOREIGN KEY ("user_id")
+    REFERENCES "users" ("id")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
 -- CreateTable
-CREATE TABLE `users` (
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `avatar` VARCHAR(255) NOT NULL,
-    `whatsapp` VARCHAR(255) NOT NULL,
-    `bio` VARCHAR(255) NOT NULL,
+CREATE TABLE "connections" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "user_id" TEXT NOT NULL,
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  CONSTRAINT "connections_user_id_fkey"
+    FOREIGN KEY ("user_id")
+    REFERENCES "users" ("id")
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
 
--- AddForeignKey
-ALTER TABLE `class_schedules` ADD CONSTRAINT `class_schedules_class_id_foreign` FOREIGN KEY (`class_id`) REFERENCES `classes`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `classes` ADD CONSTRAINT `classes_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `connections` ADD CONSTRAINT `connections_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateTable
+CREATE TABLE "users" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "avatar" TEXT NOT NULL,
+  "whatsapp" TEXT NOT NULL,
+  "bio" TEXT NOT NULL
+);
